9f579b0611773d8cabdf074affd565b40acc0558
Курьянович Егор <sontan@onca.(none)>
2011-08-14 16:32:50 +0300

    Эпизод 1: Самое начало

---
+++ lib/blog/Controller/Root.pm
@@ -0,0 +1,80 @@
+package blog::Controller::Root;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller' }
+
+#
+# Sets the actions in this controller to be registered with no prefix
+# so they function identically to actions created in MyApp.pm
+#
+__PACKAGE__->config(namespace => '');
+
+=head1 NAME
+
+blog::Controller::Root - Root Controller for blog
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 METHODS
+
+=head2 index
+
+The root page (/)
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    # Hello World
+    $c->response->body( $c->welcome_message );
+}
+
+sub list :Local {
+    my ( $self, $c ) = @_;
+    
+    # Загрузим записи
+    $c->stash( 'posts' => [$c->model('myDB::Post')->all]);
+
+    # Hello World
+    $c->stash->{template} = 'hello.tt';
+}
+
+
+=head2 default
+
+Standard 404 error page
+
+=cut
+
+sub default :Path {
+    my ( $self, $c ) = @_;
+    $c->response->body( 'Page not found' );
+    $c->response->status(404);
+}
+
+=head2 end
+
+Attempt to render a view, if needed.
+
+=cut
+
+sub end : ActionClass('RenderView') {}
+
+=head1 AUTHOR
+
+Курьянович Егор
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
